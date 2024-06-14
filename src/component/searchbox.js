564f5ecabcd29f246b3aa09f6c049df3a730e818
Vineeth Paramathmuni <vineeth2546@gmail.com>
2024-06-14 19:10:57 +0530

    initial commit

---
+++ src/component/searchbox.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function Searchbox() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
+  const [currentTask, setCurrentTask] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+
+  function addTask() {
+    if (newTask.length) {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  }
+
+  function deleteTask(index) {
+    setTasks(tasks.filter((_, i) => i !== index));
+  }
+
+  function editStart(index){
+    setIsEditing(true);
+    setEditIndex(index);
+    setCurrentTask(tasks[index])
+  }
+
+  function editTask(){
+    if(currentTask.length){
+      const updatedTasks = tasks.map((task, index) => 
+        index === editIndex ? currentTask : task)
+      setTasks(updatedTasks);
+    }
+    setIsEditing(false);
+    setCurrentTask("");
+    setEditIndex(null);
+  }
+
+  return (
+    <>
+    <h2>Todo management application</h2>
+      <input
+        type="text"
+        value={isEditing ? currentTask : newTask}
+        placeholder="Enter the task"
+        onChange={
+          (e) => { isEditing ? setCurrentTask(e.target.value) : setNewTask(e.target.value)}
+        }
+      />
+      <button onClick={isEditing ? editTask : addTask}>{isEditing ? `Update` : `Add`}</button>      
+      {tasks.map((task, index) => (
+        <p key={index}>
+          {task}
+          <button onClick={() => {deleteTask(index)}} disabled={isEditing ? true : false}>
+            Delete
+          </button>
+          <button onClick={() => {editStart(index)}}>Edit</button>
+        </p>
+      ))}
+    </>
+  );
+}
+
+
